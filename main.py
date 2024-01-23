ba0b13b7a7844406165101ed025a4dd35292b6ef
Melissa Mosser <mmosser9311@gmail.com>
2024-01-23 10:51:40 -0500

    add a bunch of stuff

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import models
+from database import engine
+from routers import auth, todos
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(auth.router)
+app.include_router(todos.router)
